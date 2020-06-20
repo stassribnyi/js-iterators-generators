4bc6da005a0bace0402a7adafd8c702a1078d642
Stas Sribnyi <stas.sribnyi@gmail.com>
2020-06-20 15:00:59 +0300

    Add custom iterable news provider

--- iterator.js
+++ iterator.js
@@ -1,3 +1,7 @@
+/**
+ * UserProvider which has custom store for users and knows how to iterate them
+ * using plain iterator boilerplate
+ */
 class UsersProvider {
   currentUserIndex = 0;
 
@@ -22,8 +26,38 @@ class UsersProvider {
   });
 }
 
-const users = new UsersProvider(['Bran', 'John', 'Stan', 'Kate']);
+/**
+ * NewsProvider which has custom store for news and knows how to iterate them
+ * using generator function to reduce amount of boilerplate required for iterator
+ */
+class NewsProvider {
+  currentNewsIndex = 0;
 
+  constructor(newsSource = []) {
+    this.newsSource = Object.entries(newsSource).reduce(
+      (users, [key, value]) => ({ ...users, [key]: value }),
+      {}
+    );
+  }
+
+  [Symbol.iterator] = function* () {
+    while (this.currentNewsIndex < Object.keys(this.newsSource).length) {
+      yield this.newsSource[this.currentNewsIndex++];
+    }
+  };
+}
+
+// Demo
+const users = new UsersProvider(['Bran', 'John', 'Stan', 'Kate']);
+const news = new NewsProvider(['Pandemic', 'Tsunami', 'Volcano']);
+
+console.log('Users:');
 for (const user of users) {
   console.log(user);
+}
+
+console.log('\nNews:');
+
+for (const newsItem of news) {
+  console.log(newsItem);
 }
